5fc0160be1056479c32bce73c7dc2f8da5b2d877
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-06-07 17:59:43 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar ('+');
+		_putchar (23);
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar ('0');
-		return (0);
+		_putchar (25);
+		return (-1);
 	}
 	else
 	{
-		_putchar ('-');
+		_putchar (28);
 		return (-1);
 	}
 }
