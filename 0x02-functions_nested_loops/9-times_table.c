75b78bdf57a6e85139f7b48de383fd1aadab4f61
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-06-07 21:22:26 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,43 @@
-#!/bin/bash
+#include"main.h"
+
+/**
+ * times_table - prints the 9 times table starting with zero
+ *
+ * Prototype: void times_table(void)
+ * Example Table
+ * 0, 0, 0, 0, ..
+ * 0, 1, 2, 3, ..
+ *start = row, end = column, d = digits of current result
+*/
+void times_table(void)
+{
+	int start, end, d;
+
+	for (start = 0; start <= 9; start++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (end = 1; end <= 9; end++)
+		{
+			d = (start * end);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (end < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+
+}
