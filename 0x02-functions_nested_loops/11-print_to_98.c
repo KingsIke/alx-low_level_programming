183b83601d7c8156446340df6c5e4c3ad4d3d45c
KINGS=IKE <69237411+KingsIke@users.noreply.github.com>
2022-06-07 23:20:34 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,37 +1,38 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_to_98 - print n to 98 counts
- *           separated by comma, followed
- *           by space and number should be
- *           printed in order
+ * print_to_98 - prints all the integers between the number given and 98
+ * @n: number to count from
  *
- * @n: input
-*/
-
-void printNos(unsigned int n)
+ * Return: void
+ */
+void print_to_98(int n)
 {
-
-    if(n > 0)
-
-    {
-
-        printNos(n - 1);
-
-        printf("%d ", n);
-
-    }
-
-    return;
-}
- 
-
-int main()
-{
-
-    printNos(100);
-
-    getchar();
-
-    return 0;
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+	else if (n < 98)
+	{
+		printf("%d", n);
+		n++;
+		while (n <= 98)
+		{
+			printf(", %d", n);
+			n++;
+		}
+		printf("\n");
+	}
+	else
+	{
+		printf("%d", n);
+		n--;
+		while (n >= 98)
+		{
+			printf(", %d", n);
+			n--;
+		}
+		printf("\n");
+	}
 }
