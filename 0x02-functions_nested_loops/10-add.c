e0c72ac62f46d712175f11bc4347efdac3cd23bd
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-06-07 21:27:18 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include"main.h"
+
+/**
+ * add - add two integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: sum of a & b
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
