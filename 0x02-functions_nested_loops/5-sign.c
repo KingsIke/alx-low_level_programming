ca63c3e70a5ce4fa39d6a0f040e2da6964cb86da
KINGS=IKE <69237411+KingsIke@users.noreply.github.com>
2022-06-07 23:45:03 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
- * print_sign - a function that prints the sign of a number
- * @n: number input
- * Return: 1 prints '+' if n > 0 || 0 prints '0' if n = 0 || -1 prints '-' if n < 0
- */
+  * print_sign - prints the sign of numbers
+  * @n: number input integer
+  * Return: 1 and prints '+' if n>0
+  *         0 and prints '0' if n=0
+  *         -1 and prints '/' if n<0
+  */
+
 int print_sign(int n)
 {
 	if (n > 0)
