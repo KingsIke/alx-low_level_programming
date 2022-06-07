8033ed4b5833da70fd9bf2b4e967a820e06930e9
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-06-07 21:44:51 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,1 +1,25 @@
-#!/bin/bash
+#include"main.h"
+
+/**
+ * main - Entry point program that computes and prints the add of all
+ *
+ * Description: computes the add of all
+ *		the multiples of 3 or 5
+ *		below 1024 (excluded)
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int add, nums;
+
+	for (nums = 0; nums < 1024; ++nums)
+	{
+		if ((nums % 3 == 0) || (nums % 5 == 0))
+			add += nums;
+	}
+	printf("%d\n", add);
+
+	return (0);
+}
