e0ac1656a549daa61d91f06ccae63ee57c3b24a9
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-06-07 17:18:28 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include"main.h"
+
+/**
+ * print_alphabet - utilizes on the _putchar function to print
+ *                 the alphabet a - z
+*/
+
+void print_alphabet(void)
+{
+	int king ;
+
+	for (king = 'a'; king <= 'z'; king++)
+		_putchar(king);
+	_putchar('\n');
+}
