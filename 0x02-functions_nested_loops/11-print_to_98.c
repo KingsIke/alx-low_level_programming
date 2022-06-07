51b8549b478e6988a352f6467d54dfbabb45a254
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-06-07 21:36:33 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * print_to_98 - print n to 98 counts
@@ -13,20 +13,31 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (; n < 98; n++)
+		while (n <= 98)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
 		}
 	}
 	else if (n > 98)
 	{
-		for (; n > 98; n--)
+		while (n >= 98)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
 		}
 	}
-	if (n == 98)
+	else
 	{
-		printf("%d\n", n);
+		printf("98");
 	}
+	printf("\n");
 }
