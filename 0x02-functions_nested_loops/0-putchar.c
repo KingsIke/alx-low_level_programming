5e3245b181669da84aa8665c4596e4731e741e1f
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-06-07 16:37:57 +0100

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,21 @@
-#!/bin/bash
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints Kingsike using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "Kingsike";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		Kingsike(str[ch]);
+	Kingsike('\n');
+
+	return (0);
+}
