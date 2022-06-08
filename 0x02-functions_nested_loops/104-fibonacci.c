5c7f810fd2911376f29503d0864e4595fd7f6792
KINGS=IKE <69237411+KingsIke@users.noreply.github.com>
2022-06-08 10:56:14 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -36,37 +36,26 @@ int numLength(int num)
  * Return: Always 0 (Success)
 */
 
-int main(void)
+
+int fib(int n)
 {
-	int count, initial0s;
-	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
-	for (count = 1; count <= 98; ++count)
-	{
-		if (f1o > 0)
-			printf("%lu", f1o);
-		initial0s = numLength(mx) - 1 - numLength(f1);
+   if (n <= 98)
 
-		while (f1o > 0 && initial0s > 0)
-		{
-			printf("%d", 0);
-			--initial0s;
-		}
+      return n;
 
-		printf("%lu", f1);
+   return fib(n-1) + fib(n-2);
+}
+ 
 
-		sum = (f1 + f2) % mx;
-		sumo = f1o + f2o + (f1 + f2) / mx;
-		f1 = f2;
-		f1o = f2o;
-		f2 = sum;
-		f2o = sumo;
+int main ()
+{
 
-		if (count != 98)
-			printf(", ");
-		else
-			printf("\n");
-	}
+  int n = 9;
 
-	return (0);
+  printf("%d", fib(n));
+
+  getchar();
+
+  return 0;
 }
