729093ed720629d360e9e9a5e81ed0a7f49841ed
KINGS=IKE <69237411+KingsIke@users.noreply.github.com>
2022-06-09 22:14:28 +0100

    Update 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase character.
+ * man isupper to learn more.
+ * @c: character
+ * Return: 1 if c is uppercase and 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if (c <= 'Z' && c >= 'A')
+	{
+		return (1);
+	}
+	return (0);
+}
