aac87d1b84a9656ce6ba4cbdee8cec76133d31c5
KINGS=IKE <69237411+KingsIke@users.noreply.github.com>
2022-06-09 22:18:31 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,10 +6,12 @@
  * @c: character
  * Return: 1 if c is uppercase and 0 otherwise.
  */
+
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c <= 'Z' && c >= 'A')
+	{
 		return (1);
-	else
-		return (0);
+	}
+	return (0);
 }
