ce487196d1e9799860b6851341891ac4e510cc8b
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-06-10 07:22:31 +0100

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include"main.h"
+#include <stdio.h>
 
 /**
  * _sqrt - finds the square root
@@ -9,61 +10,21 @@
  *
 */
 
-double _sqrt(double x)
-{
-    float sqrt, tmp;
-
-    sqrt = x / 2;
-    tmp = 0;
-
-    while (sqrt != tmp)
-    {
-        tmp = sqrt;
-        sqrt = (x / tmp + tmp) / 2;
-    }
-    return (sqrt);
-}
-
-/**
- * largest_prime_factor - finds and prints the largest
- *			prime factor of number (num)
- *
- * @num: number to find
-*/
-
-void largest_prime_factor(long int num)
-{
-    int prmNu, largest;
-
-    /* first divide with the smallest prime number (two) */
-    while (num % 2 == 0)
-        num = num / 2;
-
-    /* num must be odd so we proceed to the next prime number (plus two) */
-    for (prmNu = 3; prmNu <= _sqrt(num); prmNu += 2)
-    {
-        while (num % prmNu == 0)
-        {
-            num = num / prmNu;
-            largest = prmNu;
-        }
-    }
-
-
-    if (num > 2)
-        largest = num;
-    printf("%d\n", largest);
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
-*/
 int main(void)
 {
+	long num = 612852475143;
+	long divisor = 2;
+	long larg_prim = 0;
 
-    largest_prime_factor(612852475143);
-
-    return (0);
+	while (num != 1)
+	{
+		if (num % divisor == 0)
+		{
+			num = num / divisor;
+			larg_prim = divisor;
+		}
+		divisor += 1;
+	}
+	printf("%ld\n", larg_prim);
+	return (0);
 }
