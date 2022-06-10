00bb8b143c57990f3845bfdfcced85369e162b30
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-06-10 06:54:00 +0100

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,22 +10,20 @@
 
 void print_square(int size)
 {
-	int column, row;
+	int col, row;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (column = 1; column <= size; column++)
+		for (row = 0; row < size; row++)
 		{
-			_putchar('#');
-			for (row = 2; row <= size; row++)
+			for (col = 0; col < size; col++)
 			{
-				_putchar('#');
+				_putchar(35);
 			}
 			_putchar('\n');
 		}
 	}
+	else
+		_putchar('\n');
 }
+
