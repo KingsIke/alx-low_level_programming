da562833c489a9f843bb336572efcd96815f6c6c
KINGS=IKE <69237411+KingsIke@users.noreply.github.com>
2022-06-13 22:05:58 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,75 +5,36 @@
 #include <time.h> 
 
 /**
- * _atoi - converts a string to an integer
- *
- * @s: string input parameter
- *
- * Return: converted integer from string
+ *  101-crackme
 */
 
 void randomPasswordGeneration(int N) 
 { 
 
-    // Initialize counter 
-
     int i = 0; 
-
-  
 
     int randomizer = 0; 
 
-  
-
-    // Seed the random-number generator 
-
-    // with current time so that the 
-
-    // numbers will be different every time 
 
     srand((unsigned int)(time(NULL))); 
 
-  
-
-    // Array of numbers 
 
     char numbers[] = "0123456789"; 
 
-  
-
-    // Array of small alphabets 
-
     char letter[] = "abcdefghijklmnoqprstuvwyzx"; 
 
-  
-
-    // Array of capital alphabets 
 
     char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX"; 
 
-  
-
-    // Array of all the special symbols 
 
     char symbols[] = "!@#$^&*?"; 
 
-  
-
-    // Stores the random password 
 
     char password[N]; 
 
-  
-
-    // To select the randomizer 
-
-    // inside the loop 
 
     randomizer = rand() % 4; 
 
-  
-
-    // Iterate over the range [0, N] 
 
     for (i = 0; i < N; i++) { 
 
@@ -120,27 +81,14 @@ void randomPasswordGeneration(int N)
         } 
 
     } 
-} 
-
-  
-// Driver Code 
+}
 
 int main() 
 { 
 
-    // Length of the password to 
-
-    // be generated 
-
     int N = 10; 
 
-  
-
-    // Function Call 
-
-    randomPasswordGeneration(N); 
-
-  
+    randomPasswordGeneration(N);
 
     return 0; 
 }
