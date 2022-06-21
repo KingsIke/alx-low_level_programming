cd36ee6789b4d0d3564e408e6c14fdde4fa0af55
KINGS=IKE <69237411+KingsIke@users.noreply.github.com>
2022-06-21 21:49:10 +0100

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,12 +2,12 @@
 
 /**
  * set_string - sets the value of a pointer to a char
- * @s: pointer to change
- * @to: string to change pointer to
  *
+ * @s: double pointer
+ * @to: pointer
  * Return: void
  */
 void set_string(char **s, char *to)
 {
-    *s = to;
+	*s = to;
 }
