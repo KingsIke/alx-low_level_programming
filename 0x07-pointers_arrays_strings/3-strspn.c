1212d9382e00c77c0ebae6fd3842fb3b04aedac1
KINGS=IKE <69237411+KingsIke@users.noreply.github.com>
2022-06-21 22:09:12 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,25 +1,30 @@
 #include "main.h"
 
 /**
- * _strspn - gets lengthof a prefix substring
- * @s: string to check
- * @accept: string to check against
+ * _strspn - gets the length of a prefix substring
  *
- * Return: number of bytes of s in accept
+ * @s: string to work on
+ * @accept: pattern to match against
+ * Return: unsigned int to how many initial bytes match the pattern
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i, j;
+	unsigned int i, j, len = 0, match = 0;
 
-    for (i = 0; s[i]; i++)
-    {
-        for (j = 0; accept[j]; j++)
-        {
-            if (s[i] == accept[j])
-                break;
-        }
-        if (!accept[j])
-            break;
-    }
-    return (i);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				len++;
+				break;
+			}
+		}
+		if (len != 0)
+			match++;
+		if (match != len)
+			return (len);
+	}
+	return (0);
 }
