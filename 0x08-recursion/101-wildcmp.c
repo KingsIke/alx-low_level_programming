0b697e974021406f1970f60507be9ecbf64e0b61
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-06-22 23:47:39 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings
- * @s1: first string
- * @s2: second string, may contain wildcard *
+ * wildcmp - Compare two strings allowing for wildcard char
+ * @s1: String being compared
+ * @s2: String being compared against
  *
- * Return: 1 if match, 0 if not
+ * Return: 1 if considered identical, 0 otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-    char *s1, *s2;    
-  if( strcmp(*s1, s*2) == 0 )
-       printf("Entered strings are equal.\n");
-  else
-      printf("Entered strings are not equal.\n");
-       return 0;
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
 }
