9ca881bc3ac2fff874ee2e6c041728578e43c32a
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-06-22 23:45:44 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,22 +9,10 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	char *p;
-
-	if (*s2 == '*' && *(s2 + 1) == '\0')
-		return (1);
-	else if (*s2 == '*' && *(s2 + 1) == '*')
-		return (wildcmp(s1, s2 + 1));
-	else if (*s2 == '*')
-	{
-		p = wildcard(s1, s2 + 1);
-		if (p == 0)
-			return (0);
-		return (wildcmp(p + 1, s2 + 2));
-	}
-	else if (*s1 != *s2)
-		return (0);
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	return (wildcmp(s1 + 1, s2 + 1));
+    char *s1, *s2;    
+  if( strcmp(*s1, s*2) == 0 )
+       printf("Entered strings are equal.\n");
+  else
+      printf("Entered strings are not equal.\n");
+       return 0;
 }
