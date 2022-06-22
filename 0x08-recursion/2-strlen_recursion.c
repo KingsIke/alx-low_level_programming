deeb20f440b979062d4cf68b39f13b938793c552
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-06-22 22:58:11 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,16 +2,13 @@
 
 /**
  * _strlen_recursion - returns the length of a string
+ * @s: string to check
  *
- * @s: string
- * Return: int
+ * Return: string length
  */
 int _strlen_recursion(char *s)
 {
-    int len;
-
-    if (*s == '\0')
-        return (0);
-    len = _strlen_recursion(s + 1);
-    return (len + 1);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
