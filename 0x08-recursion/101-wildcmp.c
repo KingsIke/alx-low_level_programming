7a989d9840a2bb7b3807fee2b215e3f26dc4a19e
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-06-22 23:39:11 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,28 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * wildcmp - compares two strings
- * @s1: First string to be compared
- * @s2: Second string to be compared
- *Return: 1 if identicl, 0 if not
+ * wildcmp - Entry Point
+ * @s1: input
+ * @s2: input
+ * Return: 0
  */
 int wildcmp(char *s1, char *s2)
 {
-  int i = 0, flag = 0;
-  while(*s1 != '\0' && *s2 != '\0')
-  {
-    if(s1 != s2)
-    {
-      flag = 1;
-      break;
-    }
-    i++;
-  }
-  if(flag == 0 && *s1 == '\0' && *s2 == '\0')
-  {
-    return(1);
-  }
-
-    return(0);
-
+	return (0);
 }
