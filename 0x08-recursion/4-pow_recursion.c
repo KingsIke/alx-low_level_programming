82d63f2cd84aad15acb68979bdedd2bc249357a6
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-06-22 23:03:42 +0100

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,16 +2,16 @@
 
 /**
  * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base number
+ * @y: exponent
  *
- * @x: number to be raised
- * @y: power
- * Return: int of x to the power of y
+ * Return: x ^ y or -1 if y < 0
  */
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-        return (-1);
-    else if (y == 0)
-        return (1);
-    return (x * _pow_recursion(x, y - 1));
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
