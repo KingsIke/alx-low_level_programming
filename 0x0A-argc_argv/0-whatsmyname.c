86b6bb047e49f013712897637777e0751e78aa6d
KINGS=IKE <69237411+KingsIke@users.noreply.github.com>
2022-06-24 22:51:47 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,16 @@
-bin/bash
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints its own name
+ * @argc: argument that counts the commands passed to the program
+ * @argv: argument vector
+ * Return: An integer
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
