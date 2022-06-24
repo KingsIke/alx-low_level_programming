602908bf895f8827418c7bb55a8b0974db6f7dd0
KINGS=IKE <69237411+KingsIke@users.noreply.github.com>
2022-06-24 23:21:20 +0100

    Update _putchar.c

--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -1,1 +1,12 @@
-loving
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: Character to be passed to stdout
+ * 
+ * Return: on success 1, on error -1
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
