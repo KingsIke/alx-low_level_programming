d606c1f255dbe8ec3b62c1e0db05d12c9d57d54b
KINGS=IKE <69237411+KingsIke@users.noreply.github.com>
2022-06-24 22:53:55 +0100

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,17 @@
-bin/bash
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
