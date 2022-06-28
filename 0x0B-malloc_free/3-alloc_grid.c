b9bead6270762623ecd352c6c906bed553b544dc
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-06-28 21:37:43 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,34 +1,37 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * alloc_grid - function that creates a two dimenstional array
- * @width: the length of the array
- * @height: the height of the array
+ * alloc_grid - creates a 2 dimensional array of integers initialized to 0
+ * @width: width of array
+ * @height: height of array
  *
- * Return: the two dimensional array
- **/
+ * Return: a double pointer to the 2D array
+ */
 int **alloc_grid(int width, int height)
 {
-int **newarray;
-int i, j, k;
+	int i, j;
+	int **a;
 
-if (width <= 0 || height <= 0)
-return (NULL);
-newarray = malloc(height * sizeof(int *));
-if (newarray == NULL)
-return (NULL);
-for (i = 0; i < height; i++)
-{
-newarray[i] = malloc(width * sizeof(int));
-if (newarray[i] == NULL)
-{
-for (j = 0 ; j <= i; j++)
-free(newarray[j]);
-free(newarray);
-return (NULL);
-}
-for (k = 0; k < width; k++)
-newarray[i][k] = 0;
-}
-return (newarray);
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	a = (int **)malloc(sizeof(int *) * height);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		a[i] = (int *)malloc(sizeof(int) * width);
+		if (a[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(a[j]);
+			free(a);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+	return (a);
 }
