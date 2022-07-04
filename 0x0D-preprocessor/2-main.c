a12ac705ecbd561b74d28193e93856a7613080fe
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-07-04 21:06:42 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
   *main - prints filename.
   *
@@ -8,6 +7,6 @@
   */
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
