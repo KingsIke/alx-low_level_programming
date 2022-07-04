d89305d48dfabb7165f600a49fe457b14dcd58d7
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-07-04 21:27:43 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,14 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - information about dogs
+ * struct dog - dog structure
+ *
  * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- */
+ * @age: age of the dog
+ * @owner: dog owner
+ *
+*/
 struct dog
 {
 	char *name;
@@ -15,14 +17,13 @@ struct dog
 };
 
 /**
- * dog_t - Typedef for struct dog
- */
+ * dog_t -Typedef for struct dog
+*/
 typedef struct dog dog_t;
 
-int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /* _DOG_H_ */
+#endif /* struct dog */
