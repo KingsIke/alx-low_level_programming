a7f930e06f6bd4028b0f8de2e3dcf81f8c27d931
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-07-04 21:21:18 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,14 +2,14 @@
 #include "dog.h"
 
 /**
- * new_dog - creates a new dog
+ * dog_t - creates a new dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
  *
  * Return: pointer to new dog
  */
-dog_t *new_dog(char *name, float age, char *owner)
+dog_t *dog_t(char *name, float age, char *owner)
 {
     unsigned int nl, ol, i;
     dog_t *dog;
