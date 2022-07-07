1c3d8d24c3395ca0e749bf6f640abd76dc76cfc6
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-07-07 22:53:22 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,13 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} print_t;
+
+int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
