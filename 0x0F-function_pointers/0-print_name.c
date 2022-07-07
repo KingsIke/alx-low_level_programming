f6b2453b33c2f0e716c917a692d6263dcc19d0d8
KINGS=IKE <ogbonnayakingsike@gmail.com>
2022-07-07 23:13:27 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,15 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * print_name - function to print name
- *
- * @name: name to print
- * @f: function pointer that doesn't return anything
- *
- * Return: Always 0 (Success)
-*/
+ * print_name - function that prints a name
+ * @name: name of the person
+ * @f: input pointer function and execute it
+ * Return: nothing
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (name != 0 && f != 0)
 		f(name);
 }
